19d162c8c7406b33fead01171728c8cbdea336d8
Alejandro <morsisdivine@gmail.com>
2020-03-30 14:44:32 +1100

    base page

---
+++ app/app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var home = require('./routes/home');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/', home.home);
+
+var port =process.env.PORT || 8080;
+
+app.listen(port);
+
+module.exports = app;
