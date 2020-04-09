13c2c68906c52cd4ab5a3c1c1e4861b55a76b3b4
sirhazman07 <haroldbaldwin@hotmail.com>
2020-04-10 09:12:29 +1000

    Initial QnA Logic for office battleground, answer modals and asset files

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var http = require('http');
+var fs = require('fs');
 var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -24,3 +25,70 @@ var port =process.env.PORT || 8080;
 app.listen(port);
 
 module.exports = app;
+
+//create a server route to serve each individual html file and Json response
+var server = http.createServer(function(req,res){
+    console.log('request was made: ' + req.url);
+    if(req.url === '/covid'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/covid.html').pipe(res);
+    }
+    else if(req.url === '/office'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/office.html').pipe(res);
+    } 
+    else if(req.url === '/bus'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/bus.html').pipe(res);
+    }
+    else if(req.url === '/restaurant'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/restaurant.html').pipe(res);
+    }
+    else if(req.url === '/api/officeQuestions') 
+    {
+        var myObj = {
+            question: 'Someone looks ill, what do you do?',
+            answer1: 'User your superheroe "Distance" and keep it to yourself',
+            answer2: 'run the other way and scream in panic!',
+            solution: {
+                "correctAnswer": 'User your superheroe "Distance" and keep it to yourself'
+            }            
+        };
+        res.writeHead(200, {'Content-Type': 'application/json'});    
+        res.end(JSON.stringify(myObj))
+    }
+    else if(req.url === '/api/busQuestions') 
+    {
+        var myObj = {
+            question: 'Someone sits next you, and is begining to cough, what do you do?',
+            answer1: 'User your superheroes "Punch" and keep it to yourself',
+            answer2: 'move to another sit immediately and report it to the driver!',
+            solution: {
+                "correctAnswer": 'move to another sit immediately and report it to the driver!'
+            }
+        };
+        res.writeHead(200, {'Content-Type': 'application/json'});    
+        res.end(JSON.stringify(myObj))
+    }
+    else if(req.url === '/api/restaurantQuestions') 
+    {
+        var myObj = {
+            question: 'You notice people siting next to each otherin a restaurant, what do you do?',
+            answer1: 'User your superheroes "Sanitizer", keep a safe distance and ask them to remain at home',
+            answer2: 'move away immediately and report it to the police!',
+            solution: {
+                "correctAnswer": 'move to another sit immediately and report it to the driver!'
+            }
+        };
+        res.writeHead(200, {'Content-Type': 'application/json'});    
+        res.end(JSON.stringify(myObj))
+    }
+    else 
+    {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/404.html').pipe(res);
+    }
+});
+
+server.listen(3000, '192.168.0.17');
