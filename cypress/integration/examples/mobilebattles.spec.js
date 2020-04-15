d61165af668072ea492b02a3b772d2f691db0e54
Alejandro <morsisdivine@gmail.com>
2020-04-15 21:39:21 +1000

    change static html routing to have less code (#12)
    * change static html routing to have less code
    
    * open info links on a different tab and update mobile test

--- cypress/integration/examples/mobilebattles.spec.js
+++ cypress/integration/examples/mobilebattles.spec.js
@@ -9,15 +9,39 @@ context('COVID19 Mobile Battles', () => {
     it('Office battle', () => {      
       cy.get('#start').click()
       cy.get('#office').click()
-      cy.get('#office_title').contains('At the Office') 
+      cy.get('#staticBackdrop')
+      .should('be.visible')
+      cy.get('#start').click()
+      cy.get('#staticBackdrop')
+      .should('be.hidden')      
+      //check all elements are visible on Page
+      cy.get('#img_office')
+      .should('be.visible')
+      cy.get('#myProgress')
+      .should('be.visible')      
       cy.get('#office_question_1')
       .should('be.visible')
       cy.get('#office_answer_1')
       .should('be.visible')
       cy.get('#office_answer_2')
       .should('be.visible')
-      cy.get('#img_office')
+      //End - check all elements are visible on Page
+      
+      //Test: Select CORRECT answer button should present CORRECT modal to user
+      cy.get('#office_answer_1').click()
+      cy.get('#staticBackdrop2')
       .should('be.visible')
+      cy.get('#close_modal_btn_1').click()
+      cy.get('#staticBackdrop2')
+      .should('be.hidden')
+
+      //Test: Select CORRECT answer button should present CORRECT modal to user
+      cy.get('#office_answer_2').click()
+      cy.get('#staticBackdrop3')
+      .should('be.visible')
+      cy.get('#close_modal_btn_2').click()
+      cy.get('#staticBackdrop3')
+      .should('be.hidden')
     })
 
     it('Bus battle', () => {
