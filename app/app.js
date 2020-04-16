1ea681453e86d394c4d6413b849877ed09c856d0
Alejandro <morsisdivine@gmail.com>
2020-04-16 11:38:13 +1000

    Feature decision logic (#14)
    * Initial QnA Logic for office battleground, answer modals and asset files
    
    * added office.js Logic for question and answer, and asset imports
    
    * Added Progress Bar Logic and time out modal
    
    * Added Cypress Battler.spec.js Office Web Desktop Fixture Tests
    
    * TimeOut Modal Logic and cypress Web Tests for Timeout modal
    
    * Fixed merge conflicts
    
    * Fix Home link redirecting to incorrect page
    
    * fix app start and test
    
    * use the ubuntu box
    
    * change static html routing to have less code (#12)
    
    * change static html routing to have less code
    
    * open info links on a different tab and update mobile test
    
    * try to fix shippable flaky test
    
    * added wait
    
    * upgrade cypress
    
    * test ci chrome
    
    * fix versions
    
    * back to run on electron
    
    Co-authored-by: sirhazman07 <haroldbaldwin@hotmail.com>

--- app/app.js
+++ app/app.js
@@ -1,12 +1,18 @@
-var express = require('express');
-var http = require('http');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var home = require('./routes/home');
-
-var app = express();
+let express = require('express');
+let http = require('http');
+let fs = require('fs');
+let path = require('path');
+let logger = require('morgan');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
+let home = require('./routes/home');
+let static_html = [
+    "covid",
+    "office",
+    "bus",
+    "restaurant"
+]
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,11 +22,68 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
 app.get('/', home.home);
 
-var port =process.env.PORT || 8080;
-
+let port =process.env.PORT || 8080;
 app.listen(port);
 
 module.exports = app;
+
+static_html.forEach(function(page){
+    app.get(`/${page}`, (req, res) => {
+        fs.readFile(__dirname + `/public/${page}.html`, 'utf8', (err, text) => {
+            res.send(text);
+        });
+    });
+});
+
+//create a server route to serve each local API call until we move to use the proper API
+let server = http.createServer(function(req,res){
+    console.log('request was made: ' + req.url);
+    if(req.url === '/api/officeQuestions') 
+    {
+        let myObj = {
+            question: 'Marcus, your coworker, is constantly coughing in the office, what do you do?',
+            answer1: 'Use your superheroe Social Distance, notify your Manager and maintain a safe distance.',
+            answer2: 'Keep it to yourself, do not draw attention, and pretend you did not notice!',
+            solution: {
+                "correctAnswer": 'User your superheroe "Distance" and keep it to yourself'
+            }            
+        };
+        res.writeHead(200, {'Content-Type': 'application/json'});    
+        res.end(JSON.stringify(myObj))
+    }
+    else if(req.url === '/api/busQuestions') 
+    {
+        let myObj = {
+            question: 'Someone sits next you, and is begining to cough, what do you do?',
+            answer1: 'User your superheroes "Punch" and keep it to yourself',
+            answer2: 'move to another sit immediately and report it to the driver!',
+            solution: {
+                "correctAnswer": 'move to another sit immediately and report it to the driver!'
+            }
+        };
+        res.writeHead(200, {'Content-Type': 'application/json'});    
+        res.end(JSON.stringify(myObj))
+    }
+    else if(req.url === '/api/restaurantQuestions') 
+    {
+        let myObj = {
+            question: 'You notice people siting next to each otherin a restaurant, what do you do?',
+            answer1: 'User your superheroes "Sanitizer", keep a safe distance and ask them to remain at home',
+            answer2: 'move away immediately and report it to the police!',
+            solution: {
+                "correctAnswer": 'move to another sit immediately and report it to the driver!'
+            }
+        };
+        res.writeHead(200, {'Content-Type': 'application/json'});    
+        res.end(JSON.stringify(myObj))
+    }
+    else 
+    {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/404.html').pipe(res);
+    }
+});
+
+server.listen(3000);
---
+++ app/public/scripts/office.js
@@ -0,0 +1,115 @@
+// present the modal to user on pageload
+$( document ).ready(function() {
+    console.log( "presenting intro modal" );
+    $('#staticBackdrop').modal('show');
+    // $('.slideImg').animate({right:'0px'},1200);
+    // TODO:Enable once API's are enabled
+    // getQnAData();    
+});
+
+// Start the timer the modal to user on pageload
+$( "#start" ).click(function() {  
+    $('#office_question_1').focus();
+    startProgressBar();
+});
+
+function startProgressBar() {
+    var i = 0;
+      if (i == 0) {
+        i = 1;
+        var elem = document.getElementById("myBar");
+        var width = 1;
+        var id = setInterval(frame, 220);
+        var ary = ['#4CAF50', '#FFFF00', '#FF0000'];
+        function frame() {
+          if (width >= 100 || $('#staticBackdrop2').is(':visible')) {
+            clearInterval(id);
+            i = 0;	
+            //reset the colour to default
+            document.getElementById("myBar").style.background = "#4CAF50";
+            //show the modal only if the correct answer modal is not already on screen, 
+            if(!$('#staticBackdrop2').is(':visible')){ 
+              //hide the incorrect modal if shown on screen
+              $('#close_modal_btn_2').click();
+              $('#staticBackdrop4').modal('show');
+            }            
+          }    
+          else {      	
+            width++;
+            elem.style.width = width + "%";   
+            switch(elem.style.width) {
+                  case "40%":
+                document.getElementById("myBar").style.background = "#FFFF00";
+                break;
+                  case "75%":
+                document.getElementById("myBar").style.background = "#FF0000";
+                case "100%":
+                document.getElementById("myBar").style.width = "0";                
+            }        
+          }
+        }
+      }
+    }
+
+//Evaluate the answer and display appropiate modal
+function evaluateAnswer(textString){
+  //TODO: remove hardcoded correctanswer to submitAnswer() ajax function once API is enabled
+  // submitAnswer();
+  var correctAnswer = "Use your superheroe Social Distance, notify your Manager and maintain a safe distance.";
+  //var buttonText = $('#office_answer_1').text();
+  if(textString === correctAnswer) {
+    $('#staticBackdrop2').modal('show');
+    //return user to home page
+  } else {
+    $('#staticBackdrop3').modal('show');  
+    //TODO: return user to screen
+  }  
+}
+
+// Check the button text matches the correct answer
+$( "#office_answer_1" ).click(function() {
+  var buttonText = $('#office_answer_1').text();
+  evaluateAnswer(buttonText);      
+});
+
+$( "#office_answer_2" ).click(function() {   
+  var buttonText2 = $('#office_answer_2').text();
+  evaluateAnswer(buttonText2);       
+});
+
+///TODO:ENABLE BELOW ONCE API's ARE WORKING
+// function getQnAData (){
+//Jquery Ajax - Fetch the questions
+// $.ajax({
+//         url: "https://responsivefight.herokuapp.com/api/officeQuestions",
+//         type: 'GET',
+//         dataType: 'json', // added data type
+//         success: function(res) {
+//             console.log(res);
+  // create an object with the key of the array
+//             var json = $.parseJSON(data); 
+//             var correctAnswer = json.
+//             alert(res);
+//         }
+//     });
+// }
+
+// function submitAnswer (answer) {
+// //Jquery Ajax - Post the Answer
+// var data = [{ "response": answer}]
+// $.ajax({
+//     url: "https://responsivefight.herokuapp.com/api/officeAnswer",  
+//     type: "POST",
+//     dataType: "json",
+//     contentType: "application/json; charset=utf-8",    
+//     data: JSON.stringify(data),
+//     success: function(res){
+//       //parse response into json object
+//       var json = $.parseJSON(data);
+//     },
+//     error: function(xhr, ajaxOptions, thrownError) {
+//       //alert the user if something went wrong
+//        alert("Failed to retrieve response data! Message: " + xhr.statusText);
+//     }
+//   });
+// }
