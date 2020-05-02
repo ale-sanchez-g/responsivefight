bb84a9b7c0353a0ddaf4136bae186e76dbc67535
sirhazman07 <haroldbaldwin@hotmail.com>
2020-05-03 01:28:53 +1000

    added FE validation check for Warrior Field - TODO: update Jade to enable BS alert

--- app/public/scripts/home.js
+++ app/public/scripts/home.js
@@ -3,13 +3,19 @@ $( document ).ready(function() {
     $('#welcome_text').append(`Choose your battle field ${userName}`);
 });
 
-// Check the button text matches the correct answer
+// On click Start check for input is not null and leave create new username
 $( "#start" ).click(function() {
     let uname = document.getElementById('worrior_username').value || "guest";
-    createUser(uname);
-    console.log("user '" + uname + "' has been created");
-    localStorage.setItem("userName", uname);
-    localStorage.setItem("score", 0);
+    let username = document.getElementById('worrior_username');
+    //Check the user has entered a 3 char string in the warrior input field and it is not empty ''
+    if (uname != '' & username.length > 3) {
+        createUser(uname);
+        console.log("user '" + uname + "' has been created");
+        localStorage.setItem("userName", uname);
+        localStorage.setItem("score", 0);
+    } else {
+        alert("Please input a Warrior Name of at least 3 alphanumeric characters");        
+    }    
   });
 
 ///TODO:ENABLE BELOW ONCE API's ARE WORKING
