0679bdfef828d60a1bda3d92eb0b9cdad972b09c
Alejandro <morsisdivine@gmail.com>
2021-11-27 00:51:48 +1100

    username test

---
+++ cypress/integration/examples/user.spec.js
@@ -0,0 +1,41 @@
+/// <reference types="cypress" />
+
+context("COVID19 Battles", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:8080");
+  });
+
+  it("10 digit username", () => {
+    // Create Warrior
+    cy.get("#worrior_username").type("1234567890");
+    cy.wait(1000); // this wait is needed to synch the page
+    cy.get("#warrior").click();
+    cy.wait(1000); // this wait is needed to synch the page
+    cy.contains("1234567890");
+  });
+
+  it("11 digit username", () => {
+    // Create Warrior
+    cy.get("#worrior_username").type("12345678901");
+    cy.wait(1000); // this wait is needed to synch the page
+    cy.get("#warrior").click();
+    cy.wait(1000); // this wait is needed to synch the page
+    cy.contains("1234567890");
+  });
+
+  it("6 digit username warning", () => {
+    // Create Warrior
+    cy.get("#worrior_username").type("123456");
+    cy.wait(1000); // this wait is needed to synch the page
+    cy.contains("Only use 10 characters");
+  });
+
+  it("%5c username", () => {
+    // Create Warrior
+    cy.get("#worrior_username").type("%5c");
+    cy.wait(1000); // this wait is needed to synch the page
+    cy.get("#warrior").click();
+    cy.wait(1000); // this wait is needed to synch the page
+    cy.contains("%5c").click;
+  });
+});
