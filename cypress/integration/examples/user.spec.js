caf6152c53a44da3dec17f1a31e507582fcf57bc
Alejandro <morsisdivine@gmail.com>
2022-04-05 20:48:43 +1000

    Feature/user login (#188)
    * remove alert add flow
    
    * login feature
    
    * test and homepage logic

--- cypress/integration/examples/user.spec.js
+++ cypress/integration/examples/user.spec.js
@@ -5,10 +5,25 @@ context("COVID19 Battles", () => {
     cy.visit("http://localhost:8080");
   });
 
+  it("Register new user", () => {
+    cy.intercept("POST", "/api/registeruser", {
+      statusCode: 200,
+      body: "User created"
+    });
+
+    cy.get("#rego").click();
+    cy.get("#uname").type("test_user");
+    cy.get("#pwd").type("test_password");
+    cy.get("#psw-repeat").type("test_password");
+    cy.get("#signupbtn").click();
+    cy.get("#login").should("be.visible");
+  });
+
   it("10 digit username", () => {
     cy.get("#login").click();
     // Create Warrior
     cy.get("#worrior_username").type("1234567890");
+    cy.get("#worrior_pwd").type("pwd");
     cy.wait(1000); // this wait is needed to synch the page
     cy.get("#warrior").click();
     cy.wait(1000); // this wait is needed to synch the page
@@ -19,6 +34,7 @@ context("COVID19 Battles", () => {
     cy.get("#login").click();
     // Create Warrior
     cy.get("#worrior_username").type("12345678901");
+    cy.get("#worrior_pwd").type("pwd");
     cy.wait(1000); // this wait is needed to synch the page
     cy.get("#warrior").click();
     cy.wait(1000); // this wait is needed to synch the page
@@ -37,6 +53,7 @@ context("COVID19 Battles", () => {
     cy.get("#login").click();
     // Create Warrior
     cy.get("#worrior_username").type("%5c");
+    cy.get("#worrior_pwd").type("pwd");
     cy.wait(1000); // this wait is needed to synch the page
     cy.get("#warrior").click();
     cy.wait(1000); // this wait is needed to synch the page
