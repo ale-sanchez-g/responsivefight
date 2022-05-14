64ebd4767b3fb8715e5fab90d17232145452fa62
Alejandro <morsisdivine@gmail.com>
2022-05-14 21:51:37 +1000

    version update

--- version.sh
+++ version.sh
@@ -2,9 +2,9 @@
 
 str='
 {
-    "version": "V61",
+    "version": "63",
     "branch": "master",
-    "commitId": "270420221044"
+    "commitId": "7e8e315563dd4f7c289f255338fe59b2bc5fa102"
 }'
 
 echo "${str/'$BUILD_NUMBER'/$BUILD_NUMBER}" > app/public/version.json
