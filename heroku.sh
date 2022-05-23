424428fc6a010bd0a7372f1b67d4c854728dfe60
Alejandro <morsisdivine@gmail.com>
2022-05-23 13:37:12 +1000

    update deployment script

--- heroku.sh
+++ heroku.sh
@@ -2,4 +2,4 @@ export APP_NAME=responsivefight
 rm -rf node_modules
 docker build -t registry.heroku.com/$APP_NAME/web .
 docker push registry.heroku.com/$APP_NAME/web
-heroku container:release -a responsivefight web
+heroku container:release -a $APP_NAME web
